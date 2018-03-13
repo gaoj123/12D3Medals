761bb94553652f46358bc11a981e04bb5ef24773
Jenny Gao <jgao658@gmail.com>
2018-03-13 18:15:08 -0400

    not working yet

--- medals.js
+++ medals.js
@@ -1,11 +1,61 @@
 var sKorea=[5,8,4];
 var germany=[14,10,7];
 var slate=document.getElementById("screen");
+var ger=document.getElementById("Germany");
+var skorea=document.getElementById("SK");
+var medalCircles=d3.selectAll("circle");
 
-var circles=function(country){
-    var c=document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    slate.appendChild(c);
+var germ=function(){
+    var x=125;
+    var c1=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c1.setAttribute("cx",x);
+    c1.setAttribute("cy",250);
+    c1.setAttribute("r", 0);
+    c1.setAttribute("fill", "gold");
+    slate.appendChild(c1);
+    x+=125;
+    var c2=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c2.setAttribute("cx",x);
+    c2.setAttribute("cy",250);
+    c2.setAttribute("r", 0);
+    c2.setAttribute("fill", "silver");
+    slate.appendChild(c2);
+    x+=125;
+    var c3=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c3.setAttribute("cx",x);
+    c3.setAttribute("cy",250);
+    c3.setAttribute("r", 0);
+    c3.setAttribute("fill", "bronze");
+    slate.appendChild(c3);
+    medalCircles.data(germany);
+    medalCircles.attr("r",function(d) {return d*10;});
 };
 
-skorea.addEventListener("click", circles("SouthKorea"));
-ger.addEventListener("click", circles("Germany"));
+var southK=function(){
+    var x=125;
+    var c1=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c1.setAttribute("cx",x);
+    c1.setAttribute("cy",250);
+    c1.setAttribute("r", 0);
+    c1.setAttribute("fill", "gold");
+    slate.appendChild(c1);
+    x+=125;
+    var c2=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c2.setAttribute("cx",x);
+    c2.setAttribute("cy",250);
+    c2.setAttribute("r", 0);
+    c2.setAttribute("fill", "silver");
+    slate.appendChild(c2);
+    x+=125;
+    var c3=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c3.setAttribute("cx",x);
+    c3.setAttribute("cy",250);
+    c3.setAttribute("r", 0);
+    c3.setAttribute("fill", "bronze");
+    slate.appendChild(c3);
+    medalCircles.data(sKorea);
+    medalCircles.attr("r", function(d) {return d*10;});
+};
+
+skorea.addEventListener("click", southK);
+ger.addEventListener("click", germ);
